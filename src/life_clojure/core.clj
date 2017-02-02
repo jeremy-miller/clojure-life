3215f1ce5560a9b065fd2fe18b54fdad16d437a9
Jeremy <jeremy.miller@attackiq.com>
2017-02-01 21:54:53 -0800

    1 iteration of game working

--- src/life_clojure/core.clj
+++ src/life_clojure/core.clj
@@ -2,14 +2,8 @@
   (:require [clojure.core.matrix :refer :all])  ;; TODO only refer to required functions
   (:gen-class))
 
-;; The rules of Conway's Game of Life are as follows:
-;; 1. Any live cell with fewer than two live neighbours dies, as if caused by under-population.
-;; 2. Any live cell with two or three live neighbours lives on to the next generation.
-;; 3. Any live cell with more than three live neighbours dies, as if by over-population.
-;; 4. Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
-
-(def max-rows 4)  ;; 0-indexed
-(def max-columns 4)  ;; 0-indexed
+(def max-rows 5)
+(def max-columns 5)
 (def board [[0 0 0 0 0]
             [0 0 1 0 0]
             [0 0 1 0 0]
@@ -19,25 +13,26 @@
 (defn- get-living-neighbors
   [row column cell-value board]
   (let [row-start (max (- row 1) 0)
-        row-length (min (+ row 1) max-rows)
+        row-length (if (or (= row 0) (= row (- max-rows 1)) 2 3))  ;; subtract 1 from max-rows because it's 1-indexed
         column-start (max (- column 1) 0)
-        column-length (min (+ column 1) max-columns)]
+        column-length (if (or (= column 0) (= column (- max-columns 1)) 2 3))]  ;; subtract 1 from max-columns because it's 1-indexed
     (- (esum (submatrix board row-start row-length column-start column-length)) cell-value)))
 
 (defn- evolve
   "Return correct value of cell based on neighbors"
   [[row column] cell-value board]
   (let [live-neighbors (get-living-neighbors row column cell-value board)]
-    (if (= cell-value 1)  ;; cell is currently alive
+    (if (= cell-value 1)  ;; cell is currently living
       (cond
         (< live-neighbors 2) 0  ;; rule 1
         (> live-neighbors 3) 0  ;; rule 3
         (or (= live-neighbors 2) (= live-neighbors 3)) 1)  ;; rule 2
-      (if (= live-neighbors 3)  ;; rule 4
-        1))))
+      (if (= live-neighbors 3) 1 0))))  ;; rule 4
 
 (defn -main
   "Run the game."
   []
-  (println "Starting Conway's Game of Life...")
+  (println "Starting Conway's Game of Life...\n")
+  (pm board)
+  (println "")
   (pm (emap-indexed #(evolve %1 %2 board) board)))
